6daaa3c0e3f4481c72b166074cf63a793e63d148
Kléber de Oliveira Andrade <pdjkleber@gmail.com>
2020-07-07 15:18:05 -0300

    bug fix

---
+++ lib/components/answer_qieston.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AnswerQuestion extends StatelessWidget {
+
+  final String answer;
+  final Function onTap;
+  
+  const AnswerQuestion({
+    Key key,
+    this.answer,
+    this.onTap,
+  }) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/components/quiz_question.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+class QuizQuestion extends StatelessWidget {
+
+  final String question;
+
+  const QuizQuestion({
+    Key key,
+    this.question,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
